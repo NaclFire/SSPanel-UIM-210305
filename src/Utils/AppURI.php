6fac794754396c7c9e3f10319de027ab637e34fa
NaclFire <nacl920312@gmail.com>
2026-04-07 15:12:59 +0800

    订阅节点名调整

--- src/Utils/AppURI.php
+++ src/Utils/AppURI.php
@@ -94,14 +94,11 @@ class AppURI
                     'fp' => Tools::getRandFingerprint()
                 ];
                 $result = http_build_query($node);
-                $return = ('vless://' . $item['id'] . '@' . $item['add'] . ':' . $item['port'] . '?' . $result . '#' . urlencode($item['remark']));
+                $return = ('vless://' . $item['id'] . '@' . $item['add'] . ':' . $item['port'] . '?' . $result . '#' . rawurlencode($item['remark']));
                 break;
             case 'ss':
                 $personal_info = $item['method'] . ':' . $item['passwd'];
-                $return = 'ss://' . Tools::base64_url_encode($personal_info) . '@' . $item['address'] . ':' . $item['port'];
-                $return .= ($_ENV['add_appName_to_ss_uri'] === true
-                    ? '#' . rawurlencode($_ENV['appName'] . ' - ' . $item['remark'])
-                    : '#' . rawurlencode($item['remark']));
+                $return = 'ss://' . Tools::base64_url_encode($personal_info) . '@' . $item['address'] . ':' . $item['port'].'#' . rawurlencode($item['remark']);
                 break;
         }
         return $return;
